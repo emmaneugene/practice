6ee7691a86f1c27c9c5d240dffe7e697d0d03149
Emmanuel Oh <emmaneugene@outlook.com>
2023-02-09 09:15:24 +0800

    Blind75 - Longest Subtring Without Repeating Chars

--- leetcode/blind75/longestSubstrWoRepeatingChars.py
+++ leetcode/blind75/longestSubstrWoRepeatingChars.py
@@ -1,29 +1,50 @@
+# Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 # Given a string s, find the length of the longest substring without repeating characters.
 
+# Sliding window technique
+# Complexity O(n)
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # Corner case
         if len(s) == 0:
             return 0
 
-        result = 1
+        longest: int = 0
+        start: int = 0
+        end: int = 1
+        chars: dict = {
+            s[0]: 0
+        }
+        for i in range(1, len(s)):
+            if s[i] in chars:
+                # Check length of string
+                if i - start > longest:
+                    print(f'Found string {s[start:i]} with length {i - start}')
+                    longest = i - start
+                # Update window start
+                start = chars.get(s[i]) + 1
+                chars = dict(filter(lambda x: x[1] >= start, chars.items()))
+            
+            chars[s[i]] = i
+            
+        print("Outside loop")
+        if len(s) - start > longest:
+            print(f'Found string {s[start:len(s)]} with length {len(s) - start}')
+            longest = len(s) - start
+        return longest 
 
-        for i in range(len(s)):
-            idx = i
+def main():
+    s = Solution()
+    print(f'Actual  : {s.lengthOfLongestSubstring("abcabcbb")}')
+    print('Expected: 3')
+    print(f'Actual  : {s.lengthOfLongestSubstring("bbbbb")}')
+    print('Expected: 1')
+    print(f'Actual  : {s.lengthOfLongestSubstring("pwwkew")}')
+    print('Expected: 3')
+    print(f'Actual  : {s.lengthOfLongestSubstring("")}')
+    print('Expected: 0')
+    print(f'Actual  : {s.lengthOfLongestSubstring("abba")}')
+    print('Expected: 2')
 
-            characters = {}
-            substring_length = 0
-            repeat_found = False
-
-            while not repeat_found and idx < len(s):
-                if characters.get(s[idx]):
-                    repeat_found = True
-                else:
-                    characters[s[idx]] = 1
-                    substring_length += 1
-                idx += 1
-
-            if substring_length > result:
-                result = substring_length
-
-        return result
+if __name__ == '__main__':
+    main()
