58acbd4d9371bb449282f77ce1e1f73fdcddad61
Emmanuel Oh <emmaneugene@outlook.com>
2023-02-13 22:13:32 +0800

    Blind75: Validate BST

---
+++ leetcode/blind75/validateBST.py
@@ -0,0 +1,70 @@
+from typing import Optional
+# Problem: https://leetcode.com/problems/validate-binary-search-tree/submissions/897211926/
+
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# Time complexity: O(n)
+# Space complexity: O(n) due to recursive stack frames
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+
+        return self.checkNode(root)[0]
+
+    def checkNode(self, node: TreeNode) -> tuple[bool, int, int]:
+        '''Recursively checks whether this node is a valid BST, and returns
+        the min and max values of the BST in question
+        '''
+        if node.left is None and node.right is None:
+            return True, node.val, node.val
+        elif node.right is None:
+            leftIsBST, leftMin, leftMax = self.checkNode(node.left)
+            isBST: bool = leftIsBST and leftMax < node.val
+            return isBST, leftMin, node.val
+        elif node.left is None:
+            rightIsBST, rightMin, rightMax = self.checkNode(node.right)
+            isBST: bool = rightIsBST and rightMin > node.val
+            return isBST, node.val, rightMax
+        else:
+            leftIsBST, leftMin, leftMax = self.checkNode(node.left)
+            rightIsBST, rightMin, rightMax = self.checkNode(node.right)
+            isBST: bool = leftIsBST and leftMax < node.val \
+                and rightIsBST and rightMin > node.val
+            return isBST, leftMin, rightMax
+
+
+def main():
+    s: Solution = Solution()
+
+    root: TreeNode = TreeNode(2)
+    root.left = TreeNode(1)
+    root.right = TreeNode(3)
+    print(s.isValidBST(root))  # Expected: True
+
+    root: TreeNode = TreeNode(5)
+    root.left = TreeNode(1)
+    root.right = TreeNode(4)
+    root.right.left = TreeNode(3)
+    root.right.right = TreeNode(6)
+    print(s.isValidBST(root))  # Expected: False
+
+    root: TreeNode = TreeNode(5)
+    root.left = TreeNode(1)
+    root.right = TreeNode(6)
+    root.right.left = TreeNode(3)
+    root.right.right = TreeNode(7)
+    print(s.isValidBST(root))  # Expected: False
+
+
+if __name__ == "__main__":
+    main()
