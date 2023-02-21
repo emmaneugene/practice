8e82bd6bac32b377a22b11973a2a6c27b33e6b00
Emmanuel Oh <emmaneugene@outlook.com>
2023-02-21 19:25:33 +0800

    Blind75: Group Anagrams

---
+++ leetcode/blind75/groupAnagrams.py
@@ -0,0 +1,29 @@
+# Problem:  https://leetcode.com/problems/group-anagrams/description/
+
+# Sorting each word is nlog(n), iterating m words gives overall time complexity
+# of O(mnlog(n))
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        mappings: dict[str, list[str]] = {}
+
+        for _, s in enumerate(strs):
+            sSorted = ''.join(sorted(s))
+            if sSorted in mappings:
+                mappings[sSorted].append(s)
+            else:
+                mappings[sSorted] = [s]
+
+        return mappings.values()
+
+
+def main():
+    s: Solution = Solution()
+
+    print(s.groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']))
+    # Expected: [['bat'],['nat','tan'],['ate','eat','tea']]
+    print(s.groupAnagrams(['']))  # Expected: [['']]
+    print(s.groupAnagrams(['a']))  # Expected: [['a']]
+
+
+if __name__ == '__main__':
+    main()
