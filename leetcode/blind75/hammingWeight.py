64b3dd3d47e789bbc83abea7210a0bc251e1abe6
Emmanuel Oh <emmaneugene@outlook.com>
2023-02-26 22:48:56 +0800

    Blind 75: Hamming Weight

---
+++ leetcode/blind75/hammingWeight.py
@@ -0,0 +1,25 @@
+# Problem: https://leetcode.com/problems/number-of-1-bits
+
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count: int = 0
+        while n > 0:
+            count += n % 2
+            n //= 2
+
+        return count
+
+
+def main():
+    s: Solution = Solution()
+
+    print(s.hammingWeight(11)) # Expected: 3
+    print(s.hammingWeight(128)) # Expected: 1
+    print(s.hammingWeight(4294967293)) # Expected: 31
+
+
+if __name__ == '__main__':
+    main()
