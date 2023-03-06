8c367e667b73abc992c6e139d4d3a3d80d4bd6c7
Emmanuel Oh <emmaneugene@outlook.com>
2023-03-07 07:37:18 +0800

    Blind75: Word Break

---
+++ leetcode/blind75/wordBreak.py
@@ -0,0 +1,36 @@
+# Problem: https://leetcode.com/problems/word-break/
+
+# Dynamic programming
+
+from typing import List
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        matchedToChar: List[bool] = [False] * len(s)
+
+        wordDict.sort()
+
+        for word in wordDict:
+            if len(word) > len(s):
+                break
+            if s.startswith(word):
+                matchedToChar[len(word)-1] = True
+
+        for i in range(1, len(s)):
+            for word in wordDict:
+                if len(word) > len(s):
+                    break
+                if matchedToChar[i-1] and s[i:].startswith(word):
+                    matchedToChar[i+len(word)-1] = True
+
+        return matchedToChar[-1]
+
+def main():
+    s: Solution = Solution()
+
+    print(s.wordBreak('leetcode', ['leet', 'code'])) # Expected: True
+    print(s.wordBreak('applepenapple', ['apple', 'pen'])) # Expected: True
+    print(s.wordBreak('catsandog', ['cats', 'dog', 'sand', 'and', 'cat'])) # Expected: False
+
+if __name__ == '__main__':
+    main()
