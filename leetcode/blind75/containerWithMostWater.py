2bf59176c251be5f4c6ffebb8bd82df7c46a85a7
Emmanuel Oh <emmaneugene@outlook.com>
2023-08-24 20:22:52 +0800

    Blind75: Container with most water

--- leetcode/blind75/containerWithMostWater.py
+++ leetcode/blind75/containerWithMostWater.py
@@ -1,23 +1,37 @@
-# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
-# Find two lines that together with the x-axis form a container, such that the container contains the most water.
-# Return the maximum amount of water a container can store.
-# Notice that you may not slant the container.
+# Problem: https://leetcode.com/problems/container-with-most-water
+# Double pointers
 
 from typing import List
 
-
+# Time complexity: O(n)
+# Space complexity: O(1)
 class Solution:
-    # General idea: Start with walls of the containers as max and second max walls, make incremental improvements to find greatest possible volume.
-
-    # Once you have the above arrangement, you can either:
-    # 1) Move end pointer forward to increase volume
-    # 2) Move start pointer back to increase volume
-
     def maxArea(self, height: List[int]) -> int:
         # Base case
-        start: int = 0
-        end: int = 1
+        left: int = 0
+        right: int = len(height) - 1
 
-        maxArea: int = min(height[start], height[end]) * (end - start)
+        maxArea: int = (right-left) * min(height[left], height[right])
+
+        while (left < right):
+            # Collapse smaller of left/right inwards
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+            
+            maxArea = max(maxArea, (right-left) * min(height[left], height[right]))
 
         return maxArea
+
+def main():
+    s: Solution = Solution()
+
+    print('Expected: 49')
+    print(f'Actual  : {s.maxArea([1,8,6,2,5,4,8,3,7])}')
+
+    print('Expected: 1')
+    print(f'Actual  : {s.maxArea([1,1])}')
+
+if __name__ == '__main__':
+    main()
