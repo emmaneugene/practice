97f31f60ea4f8004c65f092218d27c4debbda43d
Emmanuel Oh <emmaneugene@outlook.com>
2023-09-11 02:38:39 +0800

    Add python template solution file

---
+++ template.py
@@ -0,0 +1,17 @@
+# Problem:
+
+# Time complexity:
+# Space complexity:
+
+from typing import Dict, List, Set
+
+class Solution:
+    pass
+
+
+def main():
+    s: Solution = Solution()
+
+
+if __name__ == '__main__':
+    main()
