4ff085e933176c7151996372af571185d3271622
Emmanuel Oh <emmaneugene@outlook.com>
2023-10-25 16:10:39 +0800

    Passed - O(n^2) solution

--- leetcode/blind75/3Sum.py
+++ leetcode/blind75/3Sum.py
@@ -1,80 +1,65 @@
 # Problem: https://leetcode.com/problems/3sum/
 
-from typing import List, Set
 
-# TODO
-
-# Time complexity: O()
-# Space complexity: O()
+# Time complexity: O(n^2)
+# Space complexity: O(n^2)
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res: Set = set()
-        
-        n: List[int]; p: List[int]; z: List[int]
-        n, p, z = [], [] , []
-         
-        for num in nums:
-            if num > 0:
-                p.append(num)
-            elif num < 0:
-                n.append(num)
-            else:
-                z.append(num)
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        results: list[list[int]] = []
 
-        N = set(N)
-        P = set(P)
+        posCounts: dict[int, int] = {}
+        negCounts: dict[int, int] = {}
+        zeroCounts: int = 0
 
-        # Handle corner cases (empty, all +ve, all -ve)
-        if ( == 0 or nums[0] > 0 or nums[-1] < 0):
-            return []
-
-        # Find "boundary", where array elements go from +ve to -ve
-        # The first 0 is treated as the boundary if it exists, otherwise smallest -ve
-        boundary: int = -1
-
-        for i in range(size):
-            if nums[i] == 0:
-                boundary = i
-                break
-            if nums[i] < 0 and nums[i+1] > 0:
-                boundary = i
-                break
-
-        # 3 sum is -ve, +ve, and 3rd value that tops up the difference (or it could be 0)
-        # -ve will always be <= boundary
-        # +ve will always be > boundary
-        # if sum -ve, look to boundary right for 3rd value (positive values)
-        # if sum +ve, look to boundary left for 3rd value (negative values)
-
-        for pos_idx in range(boundary+1, size):  # >= 0
-            for neg_idx in range(boundary+1):  # <= 0
-                intermed = nums[pos_idx] + nums[neg_idx]
-                if intermed < 0:
-                    # Search right
-                    for i in range(boundary, pos_idx):
-                        if nums[i] + intermed == 0:
-                            toAdd = [nums[neg_idx], nums[i], nums[pos_idx]]
-                            if toAdd not in output:
-                                output.append(toAdd)
-                elif intermed > 0:
-                    # Search left
-                    for i in range(boundary, neg_idx, -1):
-                        if nums[i] + intermed == 0:
-                            toAdd = [nums[neg_idx], nums[i], nums[pos_idx]]
-                            if toAdd not in output:
-                                output.append(toAdd)
+        for n in nums:
+            if n > 0:
+                if n in posCounts:
+                    posCounts[n] += 1
                 else:
-                    # Last value must be 0 (but we cannot double count)
-                    if pos_idx != boundary and neg_idx != boundary and nums[boundary] == 0:
-                        toAdd = [nums[neg_idx], nums[boundary], nums[pos_idx]]
-                        if toAdd not in output:
-                            output.append(toAdd)
+                    posCounts[n] = 1
+            elif n < 0:
+                if n in negCounts:
+                    negCounts[n] += 1
+                else:
+                    negCounts[n] = 1
+            else:
+                zeroCounts += 1
+        
+        # -ve, 0, +ve: O(n):
+        if zeroCounts > 0:
+            # Triple 0s: O(1)
+            if zeroCounts >= 3: results.append([0, 0, 0])
+            for neg in negCounts:
+                if -neg in posCounts:
+                    results.append([neg, 0, -neg])
 
-        # Last corner case: look for an occurence of [0,0,0]
-        if boundary+3 <= size and nums[boundary] == 0 and sum(nums[boundary: boundary+3]) == 0:
-            output.append([0, 0, 0])
+        # Pairs: O(n) 
+        for val, count in posCounts.items():
+            if count >= 2 and -(2 * val) in negCounts:
+                results.append([val, val, -(2 * val)])
 
-        return output
+        for val, count in negCounts.items():
+            if count >= 2 and -(2 * val) in posCounts:
+                results.append([val, val, -(2 * val)])
+
+        # All unique: O(n^2)
+        # 2 positive, 1 negative
+        positives: list[int] = list(posCounts.keys())
+        for i in range(len(positives)):
+            for j in range(i+1, len(positives)):
+                toFind: int = -(positives[i] + positives[j])
+                if toFind in negCounts:
+                    results.append([positives[i], positives[j], toFind])
+
+        # 2 negative, 1 positive
+        negatives: list[int] = list(negCounts.keys())
+        for i in range(len(negatives)):
+            for j in range(i+1, len(negatives)):
+                toFind: int = -(negatives[i] + negatives[j])
+                if toFind in posCounts:
+                    results.append([negatives[i], negatives[j], toFind])
+        
+        return results
 
 
 def main():
@@ -82,6 +67,7 @@ def main():
     print(s.threeSum([-1, 0, 1, 2, -1, -4]))  # Expected: [[-1,-1,2].[-1,0,1]]
     print(s.threeSum([0, 1, 1]))  # Expected: []
     print(s.threeSum([0, 0, 0]))  # Expected: [0,0,0]
+    print(s.threeSum([-1, -1, -1, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4]))  # Expected: [0,0,0], [-1,0,1], [-1,-1,2]
 
 
 if __name__ == '__main__':
