792c2b56b087abd6b69688c117b7b22e2f45336e
Emmanuel Oh <emmaneugene@outlook.com>
2023-11-04 17:11:18 +0800

    Add solutions

---
+++ leetcode/blind75/cloneGraph.py
@@ -0,0 +1,44 @@
+# Problem: https://leetcode.com/problems/clone-graph/
+
+# Time complexity: O(V+E)
+# Space complexity: O(V)
+
+from typing import Optional
+import collections
+
+
+class Node:
+    def __init__(self, val=0, neighbors=None) -> None:
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+        pass
+
+
+class Solution:
+    def cloneGraph(self, node: "Node") -> "Node":
+        if not node:
+            return node
+
+        q = collections.deque([node])
+        clones: dict = {node.val: Node(node.val, [])}
+
+        while q:
+            cur: Node = q.popleft()
+            cur_clone: Node = clones[cur.val]
+
+            for nb in cur.neighbors:
+                if nb.val not in clones:
+                    clones[nb.val] = Node(nb.val, [])
+                    q.append(nb)
+
+                cur_clone.neighbors.append(clones[nb.val])
+
+        return clones[node.val]
+
+
+def main():
+    s: Solution = Solution()
+
+
+if __name__ == "__main__":
+    main()
--- leetcode/blind75/numIslands.py
+++ leetcode/blind75/numIslands.py
@@ -1,28 +1,53 @@
 # Problem: https://leetcode.com/problems/number-of-islands/
 
-from typing import List
+# Time complexity: O(mn)
+# Space complexity: O(1)
 
-# TODO: Unimpl
+from collections import deque
 
 
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        rows: int = len(grid)
-        cols: int = len(grid[0])
+    def numIslands(self, grid):
+        if not grid:
+            return 0
 
-        visited: List[List[bool]] = [[False] * cols for i in range(rows)]
-        print(visited)
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
 
-        return 0
-        
+    def dfs(self, grid, i, j):
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != "1":
+            return
+        grid[i][j] = "#"
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i, j + 1)
+        self.dfs(grid, i, j - 1)
+
+
 def main():
     s: Solution = Solution()
 
-    grid: List[List[int]] = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
-    print(s.numIslands(grid)) # Expected: 1
+    grid: list[list[int]] = [
+        ["1", "1", "1", "1", "0"],
+        ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "0", "0", "0"],
+    ]
+    print(s.numIslands(grid))  # Expected: 1
 
-    grid: List[List[int]] = [["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]
-    print(s.numIslands(grid)) # Expected: 3
+    grid: list[list[int]] = [
+        ["1", "1", "0", "0", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"],
+    ]
+    print(s.numIslands(grid))  # Expected: 3
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
