7a1cd6c857d325a9f99a96c04bc762528cbe0ae0
Emmanuel Oh <emmaneugene@outlook.com>
2023-11-07 21:59:06 +0800

    Formatting and complexity analysis

--- leetcode/medium/countGoodNodesBtree.py
+++ leetcode/medium/countGoodNodesBtree.py
@@ -1,8 +1,9 @@
 # Problem: https://leetcode.com/problems/count-good-nodes-in-binary-tree/
 
-# Time complexity:
-# Space complexity:
+# Time complexity: O(n)
+# Space complexity: O(n)
 from typing import Optional
+
 
 class TreeNode:
     def __init__(self, val: int = 0, left=None, right=None) -> None:
@@ -17,32 +18,31 @@ class Solution:
 
     def countNodes(self, node: TreeNode, largest: int) -> int:
         count: int = 0
-        if node.val >= largest: 
+        if node.val >= largest:
             count += 1
             largest = node.val
 
         if node.left:
             count += self.countNodes(node.left, largest)
-        
+
         if node.right:
             count += self.countNodes(node.right, largest)
-        
+
         return count
 
 
 def main():
     s: Solution = Solution()
-    
+
     tree1 = TreeNode(3, TreeNode(1, TreeNode(3)), TreeNode(4, TreeNode(1), TreeNode(5)))
-    print(s.goodNodes(tree1)) # 4
-    
+    print(s.goodNodes(tree1))  # 4
+
     tree2 = TreeNode(3, TreeNode(3, TreeNode(4), TreeNode(2)))
-    print(s.goodNodes(tree2)) # 3
-    
+    print(s.goodNodes(tree2))  # 3
+
     tree3 = TreeNode(1)
-    print(s.goodNodes(tree3)) # 1
+    print(s.goodNodes(tree3))  # 1
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
