1236ffd805b065f1cafd5103f68347fdd77ce666
Emmanuel Oh <emmaneugene@outlook.com>
2023-12-17 12:14:48 +0800

    chore: Reorganize directories

--- algos/leetcode/blind75/alienDictionary.py
+++ algos/leetcode/blind75/alienDictionary.py
@@ -3,13 +3,14 @@
 # Time complexity:
 # Space complexity:
 
-# TODO
+# TODO - unimplemented
 
 # General idea: Check from 0th to nth character of each string to create a sorted order of chars
 # If a contradiction is encountered, return '' immediately
 # Need to maintain a queue of strings to check
 
 from typing import List
+
 
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
@@ -20,25 +21,26 @@ class Solution:
             prevCh: str = currCheck[0][0]
             order: List[str] = []
 
-            
-            for w in currCheck[1:]:
-                if currCh = 
+            # for w in currCheck[1:]:
+            #     if currCh =
             # For consecutive strings w identical characters, append to nextCheck (if there are characters remaining)
 
             # Try to insert new order into previous order, return "" if contradiction
-            
+
             # ordering: str = ''
             # for w in words:
             #     ordering += w[i]
             # print(f'depth: {i}, order {ordering}')
 
-        return ''
+        return ""
+
 
 def main():
     s: Solution = Solution()
-    print(s.alienOrder(['wrt', 'wrf', 'er', 'ett', 'rftt']))
-    print(s.alienOrder(['z', 'x']))
-    print(s.alienOrder(['z', 'x', 'z']))
+    print(s.alienOrder(["wrt", "wrf", "er", "ett", "rftt"]))
+    print(s.alienOrder(["z", "x"]))
+    print(s.alienOrder(["z", "x", "z"]))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- algos/leetcode/editDistance.py
+++ algos/leetcode/editDistance.py
@@ -14,5 +14,5 @@ def main():
     s: Solution = Solution()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
