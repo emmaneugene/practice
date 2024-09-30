5fb1616611d136d222a6025073ea803fead67996
Emmanuel Oh <emmaneugene@outlook.com>
2024-10-01 01:11:56 +0800

    AoC D2 (Java)

---
+++ algos/advent-of-code/22/02/Solution.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Please provide a file path");
+        }
+
+        Map<String, Integer> shapeVals = Map.ofEntries(
+                Map.entry("rock", 1),
+                Map.entry("paper", 2),
+                Map.entry("scissors", 3));
+
+        Map<String, String> shapeCodes = Map.ofEntries(
+                Map.entry("X", "rock"),
+                Map.entry("Y", "paper"),
+                Map.entry("Z", "scissors"));
+
+        Map<String, Integer> matchupVals = Map.ofEntries(
+                Map.entry("AX", 3), // rock - rock
+                Map.entry("AY", 6), // rock - paper
+                Map.entry("AZ", 0), // rock - scissors
+                Map.entry("BX", 0), // paper - rock
+                Map.entry("BY", 3), // paper - paper
+                Map.entry("BZ", 6), // paper - scissors
+                Map.entry("CX", 6), // scissors - rock
+                Map.entry("CY", 0), // scissors - paper
+                Map.entry("CZ", 3) // scissors - scissors
+        );
+
+        Map<String, Integer> endingVals = Map.ofEntries(
+                Map.entry("X", 0), // lose
+                Map.entry("Y", 3), // draw
+                Map.entry("Z", 6) // win
+        );
+
+        Map<String, String> outcomes2 = Map.ofEntries(
+                Map.entry("AX", "scissors"), // lose(rock) = scissors
+                Map.entry("AY", "rock"), // draw(rock) =
+                Map.entry("AZ", "paper"), // win(rock) =
+                Map.entry("BX", "rock"), // lose(paper) =
+                Map.entry("BY", "paper"), // draw(paper) =
+                Map.entry("BZ", "scissors"), // win(paper) =
+                Map.entry("CX", "paper"), // lose(scissors) =
+                Map.entry("CY", "scissors"), // draw(scissors) =
+                Map.entry("CZ", "rock") // win(scissors) =
+        );
+
+        int score1 = 0;
+        int score2 = 0;
+        String line;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+            while ((line = reader.readLine()) != null) {
+                String[] vals = line.split(" ");
+                score1 += shapeVals.get(shapeCodes.get(vals[1])) + matchupVals.get(vals[0] + vals[1]);
+
+                score2 += shapeVals.get(outcomes2.get(vals[0] + vals[1])) + endingVals.get(vals[1]);
+            }
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        System.out.println("Part 1: " + score1);
+        System.out.println("Part 2: " + score2);
+    }
+}
