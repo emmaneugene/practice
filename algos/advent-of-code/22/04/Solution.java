4709b067421ea8e73f6b4ffb933332a91559a228
Emmanuel Oh <emmaneugene@outlook.com>
2024-11-03 00:30:52 +0800

    AoC D4 (Java)

---
+++ algos/advent-of-code/22/04/Solution.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Please provide a file path");
+            return;
+        }
+
+        int score1 = 0;
+        int score2 = 0;
+        String line;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+
+            while ((line = reader.readLine()) != null) {
+                String[] assgns = line.split(",");
+                int s1 = Integer.parseInt(assgns[0].split("-")[0]);
+                int e1 = Integer.parseInt(assgns[0].split("-")[1]);
+                int s2 = Integer.parseInt(assgns[1].split("-")[0]);
+                int e2 = Integer.parseInt(assgns[1].split("-")[1]);
+
+                if ((s1 >= s2 && e1 <= e2) || (s2 >= s1 && e2 <= e1)) {
+                    score1 += 1;
+                }
+
+                if ((s1 >= s2 && s1 <= e2) || (s2 >= s1 && s2 <= e1)) {
+                    score2 += 1;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        System.out.println("Part 1: " + score1);
+        System.out.println("Part 2: " + score2);
+    }
+}
