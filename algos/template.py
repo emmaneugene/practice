9a9f0ba20a28054ca1028a075f08d3500b327e34
Emmanuel Oh <emmaneugene@outlook.com>
2025-08-04 07:56:11 +0800

    chore: Update template.py

--- algos/template.py
+++ algos/template.py
@@ -3,14 +3,14 @@
 # Time complexity: O()
 # Space complexity: O()
 
-import bisect
-import collections
-import copy
-import functools
-import heapq
-import itertools
-from dataclasses import dataclass
-from typing import Optional
+# import bisect
+# import collections
+# import copy
+# import functools
+# import heapq
+# import itertools
+# from dataclasses import dataclass
+# from typing import Optional
 
 
 class Solution:
@@ -18,7 +18,8 @@ class Solution:
 
 
 def main():
-    s = Solution()
+    # s = Solution()
+    pass
 
 
 if __name__ == "__main__":
