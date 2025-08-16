afc1eb0a44df190bd5890b83ca1b3199b7637272
Emmanuel Oh <emmaneugene@outlook.com>
2025-08-16 09:14:28 +0800

    LC 6: Zigzag conversion

---
+++ algos/leetcode/medium/zigzagConversion.py
@@ -0,0 +1,34 @@
+# Problem: https://leetcode.com/problems/zigzag-conversion/
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+from typing import List
+
+
+class Solution:
+    def convert(self, s: str, nRows: int) -> str:
+        strList: List[str] = [""] * nRows
+        chunkSize = nRows + max(0, nRows - 2)
+
+        for i in range(0, len(s), chunkSize):
+            try:
+                for j in range(nRows):
+                    strList[j] += s[i + j]
+                for k in range(nRows, chunkSize):
+                    strList[-2 - (k % nRows)] += s[i + k]
+            except IndexError:
+                break
+
+        return "".join(strList)
+
+
+def main():
+    s = Solution()
+    print(s.convert("PAYPALISHIRING", 3))  # Expected: "PAHNAPLSIIGYIR"
+    print(s.convert("PAYPALISHIRING", 4))  # Expected: "PINALSIGYAHRPI"
+    print(s.convert("A", 1))  # Expected: "A"
+
+
+if __name__ == "__main__":
+    main()
