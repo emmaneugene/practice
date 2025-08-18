dfcd352b626994f0dcb616f7f3a913a6282a4ed0
Emmanuel Oh <emmaneugene@outlook.com>
2025-08-18 09:14:28 +0800

    wip: Remaining AOC solutions

---
+++ algos/advent-of-code/22/16/Solution.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.out.println("Please provide a file path");
+            return;
+        }
+    }
+}
